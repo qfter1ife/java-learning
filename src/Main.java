1ec702b0b28107451f914453e5b2da0da36e1f3c
Stas Pavlenko <spavlenko629@gmail.com>
2024-04-18 20:03:25 +0300

    Added src/ folder

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 10;
+        for(;a<b;a++){
+            System.out.println(a);
+        }
+        System.out.println("Hello world!");
+    }
+}
