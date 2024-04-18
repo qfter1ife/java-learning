88e7359f42b47e59fdb95e30e71e11b363d1f04b
Stas Pavlenko <spavlenko629@gmail.com>
2024-04-18 20:46:12 +0300

    Added ','

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        int a = 3;
         int b = 10;
-        for(;a<b;a++){
-            System.out.println(a);
+        for(int a = 2 ;a < b;a++){
+            System.out.printf(a + ", ");
+
         }
         System.out.println("Hello world!");
     }
